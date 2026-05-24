e4413aa5dcfbacd861542b3557c2b50bb3dbf38e
vvictor1 <victoredgar_1@hotmail.com>
2026-05-23 23:24:57 -0300

    logica de resiliencia

--- index.php
+++ index.php
@@ -5,15 +5,35 @@ require_once 'Controllers/ConsultasController.php';
 
 use App\Controllers\ConsultasController;
 
+function executarOperacaoComRetry($funcao, $maxTentativas = 3) {
+    $tentativa = 0;
+    while ($tentativa < $maxTentativas) {
+        try {
+            $tentativa++;
+            return $funcao(); 
+        } catch (Exception $e) {
+            if ($tentativa >= $maxTentativas) {
+                throw $e; 
+            }
+            sleep(1); 
+        }
+    }
+}
+
+
 $controller = new ConsultasController();
 
-echo "<h2>CRIANDO CONSULTA...</h2>";
+echo "<h2>CRIANDO CONSULTA COM RESILIÊNCIA...</h2>";
 
-$resultado = $controller->create(
-    "Pedro",
-    "Dr. João",
-    "2026-05-10 14:00:00"
-);
+
+$resultado = executarOperacaoComRetry(function() use ($controller) {
+    return $controller->create(
+        "Pedro",
+        "Dr. João",
+        "2026-05-10 14:00:00"
+    );
+});
+
 
 echo "<pre>";
 print_r($resultado);
